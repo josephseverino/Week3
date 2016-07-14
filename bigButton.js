a53b27cffa8e423956aa28001ffaa689313998a4
Joseph Severino <joseph.severino@colorado.edu>
2016-07-14 19:51:32 +0000

    finished part 3 and intro to angular

---
+++ bigButton.js
@@ -0,0 +1,18 @@
+angular.module('MasterApp',[])
+
+
+angular.module('MasterApp')
+    .controller('Controller1',
+        Controller1);
+   
+   
+    console.log("no bugs so far")     
+       
+       
+        function Controller1() {
+            var myCtrl = this;
+            
+            
+        }
+
+       
