334b525c7efc9f47ed73a8259bcf86c548f6cb70
Joseph Severino <joseph.severino@colorado.edu>
2016-07-15 18:18:39 +0000

    almost finished with edit in place

---
+++ edit-in-place.js
@@ -0,0 +1,16 @@
+angular.module('Master',[])
+
+
+angular.module('Master')
+    .controller('Minion',
+        Minion);
+   
+   
+    console.log("no bugs so far")     
+       
+       
+        function Minion() {
+            var mini = this;
+            
+            
+        }
